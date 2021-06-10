b0ab7125928bb7ac5fa7e8384d0200678c938b42
leee2682 <78770258+leee2682@users.noreply.github.com>
2021-06-10 18:04:23 +0900

    Add files via upload

---
+++ home(20210610)/src/main/java/com/austin/home/controller/BoardController.java
@@ -0,0 +1,31 @@
+package com.austin.home.controller;
+
+
+import com.austin.home.model.Board;
+import com.austin.home.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    public BoardController(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
+        return "board/list";
+    }
+}

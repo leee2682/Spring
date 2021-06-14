50d7e2db68c9f0786db4e5a3fcfe2c641ff4f7d9
leee2682 <78770258+leee2682@users.noreply.github.com>
2021-06-14 17:33:53 +0900

    Add files via upload

---
+++ 20210614/home/src/main/java/com/austin/home/repository/BoardRepository.java
@@ -0,0 +1,14 @@
+package com.austin.home.repository;
+
+import com.austin.home.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
+
+
+}
